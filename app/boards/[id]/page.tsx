5b4700879b7524e3325200dd02eb5a2bc9f05ae3
WhiiteRose <tom.roger@epitech.eu>
2025-11-16 20:13:03 +0100

    [ADD] task dialog

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,10 +1,25 @@
 'use client';
 import NavBar from '@/components/navbar';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
 import { useBoard } from '@/lib/hooks/useBoards';
+import { Plus } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { useState } from 'react';
 
@@ -155,6 +170,62 @@ export default function BoardPage() {
           </div>
 
           {/* Add task dialog */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="w-full sm:w-auto">
+                <Plus />
+                Add Task
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="w-[95vw] max-w-[425px] mx-auto">
+              <DialogHeader>
+                <DialogTitle>Create New Task</DialogTitle>
+                <p className="text-sm text-gray-600">Add a task to the board </p>
+              </DialogHeader>
+              <form>
+                <div>
+                  <Label>Title *</Label>
+                  <Input id="title" name="title" placeholder="Enter task title" />
+                </div>
+                <div>
+                  <Label>Description</Label>
+                  <Textarea
+                    id="description"
+                    name="description"
+                    placeholder="Enter task description"
+                    rows={3}
+                  />
+                </div>
+                <div>
+                  <Label>Assignee</Label>
+                  <Input id="assignee" name="assignee" placeholder="Who should do this ?" />
+                </div>
+                <div>
+                  <Label>Priority</Label>
+                  <Select name="priority" defaultValue="medium">
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {['low', 'medium', 'high'].map((priority, key) => (
+                        <SelectItem key={key} value={priority}>
+                          {priority.charAt(0).toUpperCase() + priority.slice(1)}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div>
+                  <Label>Due Date</Label>
+                  <Input type="date" id="dueDate" name="dueDate" />
+                </div>
+                <div>
+                  <Button type="submit">Create Task</Button>
+                </div>
+              </form>
+            </DialogContent>
+          </Dialog>
         </div>
       </main>
     </div>
