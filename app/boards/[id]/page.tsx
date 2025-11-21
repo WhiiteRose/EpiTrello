87bd43d1771262f724c5ba0632eb91d27a33b728
WhiiteRose <tom.roger@epitech.eu>
2025-11-21 16:46:45 +0100

    [ADD] add Task button

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -21,7 +21,7 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { useBoard } from '@/lib/hooks/useBoards';
-import { ColumnWithTasks, Task } from '@/lib/supabase/models';
+import { ColumnWithTasks, Task as TaskType } from '@/lib/supabase/models';
 import { Calendar, MoreHorizontal, Plus, User } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { useState } from 'react';
@@ -57,13 +57,71 @@ function Column({
           </div>
         </div>
         {/* column content */}
-        <div className="p-2">{children}</div>
+        <div className="p-2">
+          {children}{' '}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="ghost" className="w-full mt-3 text-gray-500 hover:text-gray-700">
+                <Plus />
+                Add Task
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="w-[95vw] max-w-[425px] mx-auto">
+              <DialogHeader>
+                <DialogTitle>Create New Task</DialogTitle>
+                <p className="text-sm text-gray-600">Add a task to the board </p>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={onCreateTask}>
+                <div className="space-y-2">
+                  <Label>Title *</Label>
+                  <Input id="title" name="title" placeholder="Enter task title" />
+                </div>
+                <div className="space-y-2">
+                  <Label>Description</Label>
+                  <Textarea
+                    id="description"
+                    name="description"
+                    placeholder="Enter task description"
+                    rows={3}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label>Assignee</Label>
+                  <Input id="assignee" name="assignee" placeholder="Who should do this ?" />
+                </div>
+                <div className="space-y-2">
+                  <Label>Priority</Label>
+                  <Select name="priority" defaultValue="medium">
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {['low', 'medium', 'high'].map((priority, key) => (
+                        <SelectItem key={key} value={priority}>
+                          {priority.charAt(0).toUpperCase() + priority.slice(1)}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="space-y-2">
+                  <Label>Due Date</Label>
+                  <Input type="date" id="dueDate" name="dueDate" />
+                </div>
+                <div className="flex justify-end space-x-2 pt-4">
+                  <Button type="submit">Create Task</Button>
+                </div>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
     </div>
   );
 }
 
-function Task({ task }: { task: Task }) {
+function Task({ task }: { task: TaskType }) {
   function getPriorityColor(priority: 'low' | 'medium' | 'high'): string {
     switch (priority) {
       case 'high':
@@ -362,7 +420,12 @@ export default function BoardPage() {
 
         <div className="flex flex-col lg:flex-row lg:space-x-6 lg:overflow-x-auto lg:pb-6 lg:px-2 lg:-mx-2 lg:[&::-webkit-scrollbar]:h-2 lg:[&::-webkit-scrollbar-track]:bg-gray-100 lg:[&::-webkit-scrollbar-thumb]:bg-gray-300 lg:[&::-webkit-scrollbar-thumb]:rounded-full space-y-4 lg:space-y-0">
           {columns.map((column, key) => (
-            <Column key={key} column={column} onCreateTask={createTask} onEditColumn={() => {}}>
+            <Column
+              key={key}
+              column={column}
+              onCreateTask={handleCreateTask}
+              onEditColumn={() => {}}
+            >
               <div className="space-y-3 ">
                 {column.tasks.map((task, key) => (
                   <Task task={task} key={key} />
